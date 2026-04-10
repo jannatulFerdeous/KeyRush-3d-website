a5cbe7f7672f15e3bd0ab9a31a3cdcb5d84a04b3
jannatulFerdeous <jannatulferdeous@gmail.com>
2026-04-10 13:29:55 +0600

    update git account

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,30 @@
 import type { Metadata } from "next";
+import { Roboto_Flex } from "next/font/google";
 // import Header from "@widgets/Header";
 // import Footer from "@widgets/Footer";
-// import "../shared/styles/globals.css";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "KeyRush",
   description: "Always There for You",
 };
-
-export default function GlobalLayout({
+const robotoFlex = Roboto_Flex({
+  variable: "--font-roboto-flex",
+  subsets: ["latin"],
+  display: "swap",
+  axes: ["wdth", "slnt", "opsz"],
+});
+export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body className="no-scrollbar max-w-1440 mx-auto">
-        {/* <Header /> */}
-        <main className="min-h-screen">{children}</main>
+      <body className={`${robotoFlex.variable} antialiased`}>
+        {/* <Navbar /> */}
+        <div className="font-black-slanted">Black Slanted Text</div>
+        <main>{children}</main>
         {/* <Footer /> */}
       </body>
     </html>
